cc66916b8e982d8c3f014ab35f8dfaf147700ab1
JagadheeshRayudu <jagadheeshrayudu@gmail.com>
2023-04-26 17:25:49 +0530

    first commit

---
+++ src/main/java/com/spring/boot/mysql/MysqlSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlSpringBootApplication.class, args);
+	}
+
+}
